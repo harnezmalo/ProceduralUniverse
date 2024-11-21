d323789189097b404ea69d501732e2ebf97bf044
harnezmalo <malo.harnez@gmail.com>
2024-11-20 19:19:45 -0500

    Mouvements

---
+++ Assets/Script/CameraControler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Camera : MonoBehaviour
+{
+    public Transform SpaceShip;
+    public float latence;
+    Vector3 DistZ = new Vector3(0, 0, -2);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = SpaceShip.position + DistZ;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, latence*Time.deltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Script/SpaceshipControler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipControler : MonoBehaviour
+{
+    public float thrustForce = 5f; // Force d'accélération
+    public float rotationSpeed = 200f; // Vitesse de rotation
+    public float maxSpeed = 10f; // Vitesse maximale du vaisseau
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Récupérer les entrées du clavier
+        bool moveInput = Input.GetButton("Fire1");
+        float rotateInput = -Input.GetAxis("Horizontal"); // A / D ou Q / D (inversé pour rotation)
+
+        // Appliquer une rotation (progressive)
+        rb.AddTorque(rotateInput * rotationSpeed * Time.deltaTime);
+
+        // Appliquer une poussée vers l'avant
+        if (moveInput)
+        {
+            Vector2 force = transform.up * thrustForce;
+            rb.AddForce(force);
+
+            // Limiter la vitesse maximale
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+        }
+    }
+}
